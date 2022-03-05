8d7e6b8a4e1f0c8a0336f92d57274cb20158ad0f
CSLAB STUDENTS <studentscs@emea.local>
2022-03-05 11:06:39 +0530

    updated

--- src/components/pages/TurfDetails/ViewTurf.js
+++ src/components/pages/TurfDetails/ViewTurf.js
@@ -10,8 +10,10 @@ import {
     Grid,
     Button,
     Card,
-    Box
 } from "@mui/material";
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea, CardActions } from '@mui/material';
 import AddIcon from "@mui/icons-material/Add";
 
 export default function ViewTurf() {
@@ -41,6 +43,38 @@ export default function ViewTurf() {
                             </Button>
                         </Grid>
                     </Stack>
+                    <Grid>
+                        <Card sx={{ maxWidth: 300 }}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    height="200"
+                                    image="https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png"
+                                    alt="green iguana"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        muzammil
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        9567167713
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        muzammil@gmail.com
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        anthekkad(h),kumminipparamb(po),
+                                        malappuram(dt),kerala st
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    edit
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
                 </Container>
             </Page>
         </div>
